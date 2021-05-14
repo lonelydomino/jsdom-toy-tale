f0c20d410bff58eaaafb05be10ca5789dead2687
Michael Martinez <zeroeclipse@live.com>
2021-05-13 17:36:51 -0700

    commit tree 5bbe35d572dee9f26342ef6ea74158e5db112324

--- src/index.js
+++ src/index.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getData(toy) {
+  return fetch( 'http://localhost:3000/toys', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "name": "Jessie",
+        "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+        "likes": 0
+      })
+    } )
+    .then(function(response) {
+      return response.json()
+    } )
+    .then(function(obj) {
+      document.body.innerHTML = obj[ "id" ]
+    } )
+    .catch( function(error) {
+      document.body.innerHTML = error.message
+    } )
+}
